30d87d5b99fc4cdcfa204d61cf9b91317b2a7476
Satya Sharma <satyasharma2964@gmail.com>
2021-05-25 15:21:56 +0530

    shifting

---
+++ js/music.js
@@ -0,0 +1,7 @@
+window.addEventListener("DOMContentLoaded", event => {
+
+  // This is Code is used when audio src is passed in html boilerplate
+  const audio = document.querySelector("audio");
+  audio.volume = 0.07;
+  audio.play();
+})
---
+++ loader.js
@@ -0,0 +1,10 @@
+
+$(window).load(function() {
+		// Animate loader off screen
+    setTimeout(function(){
+        $(".se-pre-con").fadeOut("slow");
+      }, 1500);
+
+	});
+
+  
