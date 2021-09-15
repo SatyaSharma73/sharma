42b3111f0d7762a3d9f31616535e008981fe8e78
satyasharma73 <satyasharma2964@gmail.com>
2021-09-15 22:25:27 +0530

    bug fix

--- js/music.js
+++ js/music.js
@@ -2,6 +2,6 @@ window.addEventListener("DOMContentLoaded", event => {
 
   // This is Code is used when audio src is passed in html boilerplate
   const audio = document.querySelector("audio");
-  audio.volume = 0.07;
+  audio.volume = 0.2;
   audio.play();
 })
